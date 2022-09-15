24dab1e93b2309b0bb87b6368e87a7b5c15ca8e0
MinWookChoo <csc1925@naver.com>
2022-09-15 15:56:51 +0900

    Add files via upload

---
+++ 1065.py
@@ -0,0 +1,10 @@
+N = int(input())
+count = 0
+for i in range(1,N+1):
+    if i <100:
+        count+= 1
+    elif i <1000:
+        x = str(i)
+        if int(x[0]) - int(x[1]) == int(x[1]) - int(x[2]):
+            count+=1
+print(count)
